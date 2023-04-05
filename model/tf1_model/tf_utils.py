91460c35c4c6987e3edb8f02fb8241b7cb597f3d
osotsia <osotsi.a@gmail.com>
2023-04-05 10:06:13 -0400

    tf1 fixes

--- model/tf1_model/tf_utils.py
+++ model/tf1_model/tf_utils.py
@@ -18,24 +18,20 @@ def encoder_statistical(s1, embeddings, variances, thedtype=tf.float32):
   z = unPad(s1)
   embed1 = tf.nn.embedding_lookup(embeddings, z)
   tdata = tf.reduce_mean(embed1, axis=0)
-  #
-  #
+
   var1 = tf.math.abs( tf.nn.embedding_lookup(variances, z) )
   vardata = tf.reduce_sum(var1, axis=0)/tf.square( tf.cast(tf.shape(var1)[0], dtype=thedtype) ) 
   return tf.stack([tdata, vardata], axis=1)
-
 
 
 def updateRvector(R, predprobs, lambdaval, tnow):
   H = constant_hazard(lambdaval, tnow)
   growthprobs = R + predprobs + tf.math.log(1-H)
   changeprobs = tf.math.reduce_logsumexp( R + predprobs + tf.math.log(H) )
-  #
-  #
+
   Rprime = tf.concat([tf.reshape(changeprobs, (1,)), growthprobs], axis=0)
   Rprime = tf.reshape(Rprime, (tnow+1,))
   return log_normalize(Rprime)
-
 
 
 def get_predprobs(muT, TT, vardata, tdata, thedtype=tf.float32):
@@ -51,12 +47,10 @@ def run_iteration(package, vardata, tdata, lambdaval, mu0, T0, tnow):
   [Ra, muT, TT] = package
   with tf.name_scope('Predprobs') as scope:
     predprobs1 = get_predprobs(muT, TT, vardata[tnow-1], tdata[tnow-1])
-  #
-  #
+
   with tf.name_scope('UpdateR') as scope:
     Rb = updateRvector(Ra, predprobs1, lambdaval, tnow)
-  #
-  #
+
   with tf.name_scope('UpdatePar') as scope:
     TT0a = solve( solve(TT) + solve(vardata[tnow-1]) )
     muT0a = tf.multiply(TT0a , tf.multiply(solve(vardata[tnow-1]), tdata[tnow-1]) + tf.multiply(solve(TT), muT ) ) 
